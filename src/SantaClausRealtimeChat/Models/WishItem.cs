5fdc68db905e8d1dbd4d6f612fb753a1108e17e0
Sebastian Jensen <apps@tsjdev-apps.de>
2024-12-10 21:44:30 +0100

    Create WishItem.cs

---
+++ src/SantaClausRealtimeChat/Models/WishItem.cs
@@ -0,0 +1,8 @@
+﻿namespace SantaClausRealtimeChat.Models;
+
+/// <summary>
+///     Represents an item on a wish list with a name and an array of wishes.
+/// </summary>
+/// <param name="Name">The name of the wish item.</param>
+/// <param name="Wishes">An array of wishes associated with the wish item.</param>
+internal record class WishItem(string Name, string[] Wishes);
