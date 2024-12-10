3041d420ae17da6cac8d051038a36fe90b6574e9
Sebastian Jensen <apps@tsjdev-apps.de>
2024-12-10 21:44:45 +0100

    Create Statics.cs

---
+++ src/SantaClausRealtimeChat/Utils/Statics.cs
@@ -0,0 +1,50 @@
+﻿namespace SantaClausRealtimeChat.Utils;
+
+/// <summary>
+///     Provides static constants for various 
+///     (Azure) OpenAI models and keys.
+/// </summary>
+internal static class Statics
+{
+    /// <summary>
+    ///     The name for Azure OpenAI.
+    /// </summary>
+    public const string AzureOpenAI 
+        = "Azure OpenAI";
+
+    /// <summary>
+    ///     The name for OpenAI.
+    /// </summary>
+    public const string OpenAI 
+        = "OpenAI";
+
+    /// <summary>
+    ///     The name for the OpenAI real-time model.
+    /// </summary>
+    public const string OpenAIRealtimeModelName 
+        = "gpt-4o-realtime-preview";
+
+    /// <summary>
+    ///     The key for GPT-4o.
+    /// </summary>
+    public const string GPT4oKey 
+        = "gpt-4o";
+
+    /// <summary>
+    ///     The key for GPT-4o mini.
+    /// </summary>
+    public const string GPT4oMiniKey 
+        = "gpt-4o-mini";
+
+    /// <summary>
+    ///     The key for GPT-4-Turbo.
+    /// </summary>
+    public const string GPT4TurboKey 
+        = "gpt-4-turbo";
+
+    /// <summary>
+    ///     The key for GPT-4.
+    /// </summary>
+    public const string GPT4Key 
+        = "gpt-4";
+}
